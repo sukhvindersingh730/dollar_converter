d27c41923d0967d4a878a192d49d09ced6ba7229
Sukhvinder Singh <147253173+sukhvindersingh730@users.noreply.github.com>
2023-10-07 18:51:15 +0530

    Add files via upload

---
+++ BMI/doller.js
@@ -0,0 +1,6 @@
+function honey(){
+    let  d=document.getElementById('dollar').value;
+    let d1=d*83.18;
+    let result=d1;
+    document.getElementById('btn1').innerHTML="INDIAN RUPEES= " + result
+}
